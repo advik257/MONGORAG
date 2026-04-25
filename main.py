83d1706ac19b9940766070335329014610e4bac1
advik257 <Adviktest99@gmail.com>
2026-04-25 15:15:25 +0530

    logging setup done

--- main.py
+++ main.py
@@ -3,6 +3,25 @@ from exceptions import *
 import boto3
 logger = boto3.client("logs")
 
+# # main.py
+# from logger import setup_logging
+# setup_logging()          # ✅ call ONCE at startup
+
+# # pdf_processor.py
+# from logger import get_logger
+# logger = get_logger(__name__)    # → rag_app.pdf_processor
+# logger.info("Loading PDF...")
+
+# # embeddings.py
+# from logger import get_logger
+# logger = get_logger(__name__)    # → rag_app.embeddings
+# logger.info("Generating embeddings...")
+
+# # mongo.py
+# from logger import get_logger
+# logger = get_logger(__name__)    # → rag_app.mongo
+# logger.error("Insert failed!")
+
 def main():
     print("Hello from ragmongodb!")
 
